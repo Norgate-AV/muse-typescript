0bd73415eac7d4318abe9b9902a95deb4d37bd35
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-11-28 11:18:34 +0000

    feat: updates

---
+++ lib/SNAPI.ts
@@ -0,0 +1,43 @@
+export default {
+    PLAY: 1,
+    STOP: 2,
+    PAUSE: 3,
+    FFWD: 4,
+    REW: 5,
+    SFWD: 6,
+    SREV: 7,
+    RECORD: 8,
+    POWER: 9,
+
+    VOL_UP: 24,
+    VOL_UP_FB: 24,
+    VOL_DN: 25,
+    VOL_DN_FB: 25,
+    VOL_MUTE: 26,
+
+    PWR_ON: 27,
+    PWR_OFF: 28,
+
+    FRAME_FWD: 185,
+    FRAME_REV: 186,
+    SLOW_FWD: 188,
+    SLOW_REV: 189,
+    SCAN_SPEED: 192,
+
+    VOL_MUTE_ON: 199,
+    VOL_MUTE_FB: 199,
+
+    PLAY_FB: 241,
+    STOP_FB: 242,
+    PAUSE_FB: 243,
+    SFWD_FB: 246,
+    SREV_FB: 247,
+    RECORD_FB: 248,
+    SLOW_FWD_FB: 249,
+    SLOW_REV_FB: 250,
+
+    DEVICE_COMMUNICATING: 251,
+    DATA_INITIALIZED: 252,
+    POWER_ON: 255,
+    POWER_FB: 255,
+} as const;
--- src/app.ts
+++ src/app.ts
@@ -45,6 +45,8 @@ function tpOnlineEventCallback(): void {
 
     tpFeedbackSetup();
 
+    writeToFile();
+
     tp.port[1].send_command("@PPX");
     tp.port[1].send_command("ADBEEP");
     tp.port[1].send_command("PAGE-Logo");
@@ -75,6 +77,42 @@ function tpFeedbackHandler(): void {
         const { port, code } = source.button.channel;
         tp.port[port].channel[code] =
             state.selectedSource.button.channel.code === code;
+    }
+}
+
+function writeToFile(): void {
+    const File = Java.type("java.io.File");
+    const Files = Java.type("java.nio.file.Files");
+    const Paths = Java.type("java.nio.file.Paths");
+    const FileWriter = Java.type("java.io.FileWriter");
+    const System = Java.type("java.lang.System");
+
+    const path = Paths.get("/mnt/data/mojo/mojo/program/muse-typescript");
+    const entities = Files.list(Paths.get(`${path}`));
+
+    context.log.info(`CWD: ${System.getProperty("user.dir")}`);
+    context.log.info(`Home: ${System.getProperty("user.home")}`);
+
+    entities.forEach((entity) => {
+        context.log.info(entity.toString());
+    });
+
+    try {
+        const file = new File(`${path}/file.txt`);
+        if (file.createNewFile()) {
+            context.log.info("File created: " + file.getName());
+        } else {
+            context.log.info("File already exists.");
+        }
+
+        const writer = new FileWriter(`${path}/file.txt`);
+        writer.write("Hello, World!\n");
+        writer.close();
+
+        const data = Files.readAllLines(Paths.get(`${path}/file.txt`));
+        context.log.info(data);
+    } catch (error: any) {
+        context.log.error(error);
     }
 }
 
