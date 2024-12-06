bd6aba661a80803a0a646a224154a4d48d59ce8b
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-12-06 22:05:33 +0000

    feat: updates

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-// if (global) {
-//     global._nashornPolyfill = true;
-// }
-var console: any = {};
-console.debug = print;
-console.warn = print;
-console.log = print;
-
+import "core-js/es/array/find";
+import "core-js/es/array/find-index";
 import "core-js/es/array/from";
 import "core-js/es/array/includes";
+import "core-js/es/object/assign";
+import "core-js/es/object/create";
+import "core-js/es/object/entries";
 import "core-js/es/object/keys";
+import "core-js/es/object/values";
+import "core-js/es/string/includes";
+import "core-js/es/string/pad-end";
+import "core-js/es/string/pad-start";
 
-import { main } from "./main.js";
+import { main } from "./main";
 
 // Start the program
 main();
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import App from "./App";
 
-export function main() {
-    const app = new App().init();
+function panic(message: string) {
+    throw new Error(message);
+}
 
-    if (!app) {
-        throw new Error("App failed to initialize");
+export function main() {
+    try {
+        const app = new App().init();
+
+        if (!app) {
+            throw new Error("App failed to initialize");
+        }
+    } catch (error: any) {
+        panic(error.message);
     }
 
-    console.log("Program Started");
+    context.log("Program Started");
 }
