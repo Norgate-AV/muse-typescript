bbf6dd67f38de055e3068be7f02a2dd0dbeedb07
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2024-12-17 07:54:39 +0000

    feat: updates

--- src/index.ts
+++ src/index.ts
@@ -24,20 +24,16 @@ if (!globalThis.console) {
     globalThis.console.error = context.log.error;
 }
 
-// import "core-js/es/array/find";
-// import "core-js/es/array/find-index";
-// import "core-js/es/array/from";
-// import "core-js/es/array/includes";
+// Polyfills
 import "core-js/es/array";
 import "core-js/es/array-buffer";
 import "core-js/es/date";
-// import "core-js/es/error";
-// import "core-js/es/function";
-// import "core-js/es/get-iterator";
+import "core-js/es/error";
+import "core-js/es/function";
+import "core-js/es/get-iterator";
 import "core-js/es/global-this";
-// import "core-js/es/instance";
 import "core-js/es/iterator";
-// import "core-js/es/json";
+import "core-js/es/json";
 import "core-js/es/map";
 import "core-js/es/math";
 import "core-js/es/number";
@@ -46,25 +42,12 @@ import "core-js/es/object/create";
 import "core-js/es/object/entries";
 import "core-js/es/object/keys";
 import "core-js/es/object/values";
-// import "core-js/es/object";
 import "core-js/es/promise";
 import "core-js/es/reflect";
 import "core-js/es/regexp";
 import "core-js/es/set";
-import "core-js/es/string/includes";
-import "core-js/es/string/pad-end";
-import "core-js/es/string/pad-start";
-// import "core-js/es/string";
-import "core-js/es/typed-array/uint8-array";
-import "core-js/es/typed-array/uint8-clamped-array";
-import "core-js/es/typed-array/uint16-array";
-import "core-js/es/typed-array/uint32-array";
-import "core-js/es/typed-array/int8-array";
-import "core-js/es/typed-array/int16-array";
-import "core-js/es/typed-array/int32-array";
-import "core-js/es/typed-array/float32-array";
-import "core-js/es/typed-array/float64-array";
-// import "core-js/es/typed-array";
+import "core-js/es/string";
+import "core-js/es/typed-array";
 import "core-js/es/weak-map";
 import "core-js/es/weak-set";
 
